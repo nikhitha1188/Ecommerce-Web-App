87847d729e24152b82a91f16af64fdcafca3358d
nikhitha1188 <yarajarlanikhitha@gmail.com>
2026-02-08 14:42:40 +0530

    Initial commit

---
+++ src/main/java/com/codegnan/cgecom/JspwithbootApplication.java
@@ -0,0 +1,12 @@
+package com.codegnan.cgecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspwithbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspwithbootApplication.class, args);
+	}
+}
